cba2955eb9ef8871c8f561c83e116f9def28386d
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-01-27 15:45:48 +0700

    Update swk.py

--- swk.py
+++ swk.py
@@ -80,9 +80,9 @@ async def fetch(session, url):
             r += 1
           reqs.append(response.status)
           print()
-          sys.stdout.write(f"\033[48;5;7m\033[30m•> Website\033[0m \033[32m{str(url)}\033[38;5;39mRequests\033[37m=> {str(len(reqs))}\r")
-          sys.stdout.write(f"\033[37m•> Website \033[37m{str(url)} \033[38;5;39mtime \033[33m=> {final}\r")
-          sys.stdout.write(f"\033[38;5;37m•> Website \033[38;5;166m{str(url)} \033[38;5;39mStatus Code \033[32m=> {str(response.status)}\r") 
+          sys.stdout.write(f"\033[48;5;7m\033[30mWebsite\033[0m \033[32m{str(url)}\033[38;5;39m Requests\033[37m•> {str(len(reqs))}\r")
+          sys.stdout.write(f"\033[37mWebsite \033[37m{str(url)} \033[38;5;39mtime \033[33m•> {final}\r")
+          sys.stdout.write(f"\033[38;5;37mWebsite \033[38;5;166m{str(url)} \033[38;5;39mStatus Code \033[32m•> {str(response.status)}\r") 
         else:
           print(Colorate.Horizontal(Colors.red_to_green, "[-] Server is not responding"))
 
