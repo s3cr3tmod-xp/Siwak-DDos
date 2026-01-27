7506e4c600ca5b72a542e36bc647a09cec79d698
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-01-27 16:44:48 +0700

    Update swk.py

--- swk.py
+++ swk.py
@@ -62,11 +62,11 @@ banner = r"""
 print(Colorate.Horizontal(Colors.green_to_red, banner))
 # Version and URL
 banner = """
-╔{'═' * 68}╗")
-║\033[48;5;4m{' ' * 22}\033[37mA u t h o r :  K u n F z{' ' * 21} \033║")
-║\033[48;5;4m{' ' * 19}\033[37mA d m :  Z' b l a c k  3 1 3{' ' * 20} \033[0m║")
-║\033[48;5;4m{' ' * 10}\033[37mC a n  b e  u s e d  b u t  n o t  s h a r e d{' ' * 11} \033[0m║")
-╚{'═' * 68}╝")
+╔{'═' * 68}╗
+║\033[48;5;4m{' ' * 22}\033[37mA u t h o r :  K u n F z{' ' * 21} \033║
+║\033[48;5;4m{' ' * 19}\033[37mA d m :  Z' b l a c k  3 1 3{' ' * 20} \033[0m║
+║\033[48;5;4m{' ' * 10}\033[37mC a n  b e  u s e d  b u t  n o t  s h a r e d{' ' * 11} \033[0m║
+╚{'═' * 68}╝
 """
 print(Colorate.Horizontal(Colors.green_to_red, versi and URL))
 
