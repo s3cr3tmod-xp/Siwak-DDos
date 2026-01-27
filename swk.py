460d4851e5da573fcdb9ce7b8d76ff6f7bf8730f
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-01-27 11:58:37 +0700

    Update swk.py

--- swk.py
+++ swk.py
@@ -49,7 +49,7 @@ banner = r"""
         """.replace('▒▒', '▒▒')
 
 
-print(Colorate.Horizontal(Colors.pink_to_blue, banner))
+print(Colorate.Horizontal(Colors.green_to_red, banner))
 #//Gui End//#
 num = 0
 reqs = []
