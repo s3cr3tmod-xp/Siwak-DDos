dc0f9e23583c3cd549ec0b475310fb6721a5ee0e
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-01-30 10:48:29 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("pip install requests")
+    os.system("pip install multiprosess")
+    os.system("pip install pystyle")
+    os.system("pip install colorama")
+elif c == "1":
+    os.system("pip3 install requests")
+    os.system("pip3 install multiprosess")
+    os.system("pip3 install pystyle")
+    os.system("pip3 install colorama")
+print("Done.")
