3c0ddfea7179e15c6b2e3226b8e63265e00707e9
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-01-30 12:57:05 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,9 @@ c = input(">>>: ")
 if c == "0":
     os.system("pip install requests")
     os.system("pip install multiprocess")
-    os.system("pip install pystyle")
+    
     os.system("pip install aiohttp")
 elif c == "1":
     os.system("pip3 install requests")
     os.system("pip install multiprocess")
-    os.system("pip3 install pystyle")
-    os.system("pip3 install aiohttp")
 print("Done.")
